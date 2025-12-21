fa6fa94e64cd7aca12dbadd27345372da5fc924b
DenisHerzhyk <denis.herzhyk88@gmail.com>
2025-12-21 17:27:17 +0200

    Add layout, index page, add celery

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -27,7 +26,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Application definition
 
 INSTALLED_APPS = [
@@ -54,8 +57,9 @@ ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATES = [
     {
+
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
